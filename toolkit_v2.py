7d2fd8667acb6bef8d10cf94cbcfaa850aa3816a
kazutomita-svg <kazu_tomita@paraparaumucollege.school.nz>
2026-04-30 10:29:39 +1200

    Add files via upload

--- toolkit_v2.py
+++ toolkit_v2.py
@@ -2,16 +2,16 @@
 import tkinter as tk
 from tkinter import ttk
 
-# 1. Setup the window
+# Setup the window
 root = tk.Tk()
 root.title("Robotics Toolkit V2 - Tabbed Interface")
 root.geometry("400x300")
 
-# 2. Logic: Shared function for testing
+# Output function when button is pressed
 def on_click(label_to_update):
     label_to_update.config(text="This is Version 2", fg="blue")
 
-# 3. Create the Tab Controller (Notebook)
+# Tab controller
 notebook = ttk.Notebook(root)
 tab1 = ttk.Frame(notebook)
 tab2 = ttk.Frame(notebook)
@@ -23,7 +23,7 @@ notebook.pack(expand=1, fill="both")
 # --- Content for Tab 1 ---
 label1 = tk.Label(tab1, text="This is Tab 1", font=("Arial", 12))
 label1.pack(pady=20)
-# Note: We use a 'lambda' so we can tell the function which label to change
+# Note: 'lambda' is used so we can tell the function which label to change
 btn1 = tk.Button(tab1, text="Press the button", command=lambda: on_click(label1))
 btn1.pack(pady=10)
 
@@ -33,5 +33,5 @@ label2.pack(pady=20)
 btn2 = tk.Button(tab2, text="Press the button", command=lambda: on_click(label2))
 btn2.pack(pady=10)
 
-# 4. Start the application
+# Start the code
 root.mainloop()
